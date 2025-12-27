30f3b8618574c9b6577cc9b1d4674511cf25406d
mfkahraman <mfurkankahraman@gmail.com>
2025-12-27 23:44:05 +0300

    Add project files.

---
+++ OrderService/OrderService.Domain/Common/AggregateRoot.cs
@@ -0,0 +1,20 @@
+﻿namespace OrderService.Domain.Common
+{
+    public abstract class AggregateRoot : Entity
+    {
+        private readonly List<DomainEvent> _domainEvents = new();
+
+        public IReadOnlyCollection<DomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+        protected void AddDomainEvent(DomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+
+        public void ClearDomainEvents()
+        {
+            _domainEvents.Clear();
+        }
+    }
+
+}
---
+++ OrderService/OrderService.Domain/Common/DomainEvent.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace OrderService.Domain.Common
+{
+    public abstract class DomainEvent : INotification
+    {
+        public DateTime OccurredOn { get; protected set; } = DateTime.UtcNow;
+    }
+}
